d226cd13c7440f79b05b4f06ac4cb27073714a00
Langery <1245066197@qq.com>
2020-01-09 17:00:34 +0800

    merge

--- PyTorch/Automatic_derivation.py
+++ PyTorch/Automatic_derivation.py
@@ -15,14 +15,24 @@ a = x + y
 b = a + z
 # print(b.requires_grad) # True
 
-model = torchvision.models.resnet18(pretrained = True)
-# print(models)
-for param in model.parameters():
-    param.requires_grad = False
-model.fc = nn.Linear(512, 100)
+# ======================> requires_grad
+# model = torchvision.models.resnet18(pretrained = True)
+# # print(models)
+# for param in model.parameters():
+#     param.requires_grad = False
+# model.fc = nn.Linear(512, 100)
 
-# print(models)
+# print(model)
+# print(nn.Linear(512, 100))
 
 # OPTIMIZE:
-optimizer = optim.SGD(model.fc.parameters(), lr=1e-2, momentum=0.9)
-print(optimizer)
+# optimizer = optim.SGD(model.fc.parameters(), lr=1e-2, momentum=0.9)
+# print(optimizer)
+
+# ======================> volatile
+regular_input = Variable(torch.randn(5, 5))
+# volatile_input = Variable(torch.randn(5, 5), torch.no_grad())
+with torch.no_grad():
+    volatile_input = Variable(torch.randn(5, 5))
+model = torchvision.models.resnet18(pretrained=True)
+print(model)
