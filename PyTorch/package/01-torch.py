892d850f822abb588fdd81c7ec33b15131a5f25d
Langery <1245066197@qq.com>
2020-01-26 21:18:33 +0800

    merge

--- PyTorch/package/01-torch.py
+++ PyTorch/package/01-torch.py
@@ -1,7 +1,16 @@
-import torch
+import torch, numpy
+import numpy as np
 
 # Creation Ops
 # Two-dimensional tensor
 a = torch.eye(3)
 # a = torch.eye(5)
 print(a)
+
+b = np.array([1, 2, 3])
+t = torch.from_numpy(b)
+print(t)
+
+torch.LongTensor([1, 2, 3])
+t[0] = -1
+print(b)
