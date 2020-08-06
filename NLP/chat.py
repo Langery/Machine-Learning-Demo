fef31b106300465948d40966a50b50f1ed4dc5d2
Langery <1245066197@qq.com>
2020-08-06 10:03:27 +0800

    merge chat

--- NLP/chat.py
+++ NLP/chat.py
@@ -1,15 +1,7 @@
 import time
 import calendar
 
-dict = {
-  "你好": "你好",
-  "很高兴见到您": "我也很高心见到你",
-  "你喜欢吃什么水果": "我喜欢橘子",
-  "你今年多大了": "18岁了",
-  "你很漂亮": "谢谢",
-  # "现在的时间": "你好",
-  # "这个月的阳历": "你好",
-}
+from dict.chatDict import dict
 
 flag = ('A')
 work = True
@@ -17,13 +9,14 @@ print('你好，我是你的机器人助手\n')
 print('请问有时间和我聊聊么\n')
 
 while flag == 'A' or 'B':
-  flag = input('A:陪我聊天,B:训练我聊天,C:给我翻译,D:天气,E:结束\n')
+  flag = input('A:陪我聊天,B:训练我聊天,C:给我翻译,D:天气,E:结束\nX:查看字典\n')
   if flag == "B":
       question = input('请输入你想问的\n')
       answer = input('请输入问题的答案\n')
       dict[str(question)] = str(answer)
       print('学习成功\n')
       print('现在我已经学会了%d个问题\n'%len(dict))
+      # 将训练后的数据写入到字典中
       continue
   elif flag == 'A':
       if len(dict) == 0:
@@ -41,9 +34,8 @@ while flag == 'A' or 'B':
       if work == False:
           print('sorry，这个问题我回答不上来\n')
           work = True
-      elif flag == 'E':
-          print('好的，我们下次再聊\n')
-          break
-      else:
-          continue
-      # elif flag == 'C':
+  elif flag == 'E':
+      print('好的，我们下次再聊\n')
+      break
+  elif flag == 'X':
+      print(dict)
